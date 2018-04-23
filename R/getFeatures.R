7c1279ac7a026b0da4c031cef9ddb1532cf70237
Caleb Brooks <calebmbrooks@gmail.com>
2018-04-23 10:51:21 -0400

    Modified getFeatures(), deleted getAudioFeatures()

--- R/getFeatures.R
+++ R/getFeatures.R
@@ -1,12 +1,15 @@
-#'Get audio feature information for a single track identified by its unique Spotify ID.
+#'Get audio feature information for up to 100 tracks identified by their unique Spotify IDs.
 #'
 #'
 #'function to get audio feature information for a single track identified by its unique Spotify ID.
-#'@param spotify_ID The Spotify ID for the track.
+#'@param spotify_ID The Spotify ID for the track, or a vector of track IDs
 #'@param token An OAuth token created with \code{spotifyOAuth}.
 #'@export
 
 getFeatures<-function(spotify_ID,token){
+  if(is.vector(spotify_ID)){
+    id <- paste(spotify_ID, collapse=',')
+  }
   req <- httr::GET(paste0("https://api.spotify.com/v1/audio-features/",spotify_ID), httr::config(token = token))
   json1<-httr::content(req)
   dados=data.frame(id=json1$id,
