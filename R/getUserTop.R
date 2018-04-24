ba349e671fb52e4c105d5e37f5ad8d9309a16dd1
Caleb Brooks <calebmbrooks@gmail.com>
2018-04-24 01:36:39 -0400

    Added a bunch of unlist() calls

--- R/getUserTop.R
+++ R/getUserTop.R
@@ -16,21 +16,21 @@ getUserTop<-function(artists=T,token){
   json1<-httr::content(req)
   json2<-jsonlite::fromJSON(jsonlite::toJSON(json1))$items
   if(artists){
-    dados=data.frame(id=json2$id,
-                     name=json2$name,
-                     popularity=json2$popularity,
-                     followers=json2$followers,
-                     genres=paste(json2$genres,collapse =","))
+    dados=data.frame(id=unlist(json2$id),
+                     name=unlist(json2$name),
+                     popularity=unlist(json2$popularity),
+                     followers=unlist(json2$followers),
+                     genres=paste(unlist(json2$genres),collapse =","))
   }
   else{
-    dados=data.frame(id=json2$id,
-                     name=json2$name,
-                     explicit=json2$explicit,
-                     popularity=json2$popularity,
-                     artists = paste(lapply(json2$artists, function(x) x$name), collapse = ";"),
-                     artists_IDs=paste(lapply(json2$artists, function(x) x$id), collapse = ";"),
-                     album=json2$album$name,
-                     albumID=json2$album$id)
+    dados=data.frame(id=unlist(json2$id),
+                     name=unlist(json2$name),
+                     explicit=unlist(json2$explicit),
+                     popularity=unlist(json2$popularity),
+                     artists = paste(lapply(unlist(json2$artists), function(x) x$name), collapse = ";"),
+                     artists_IDs=paste(lapply(unlist(json2$artists), function(x) x$id), collapse = ";"),
+                     album=unlist(json2$album$name),
+                     albumID=unlist(json2$album$id))
 
   }
   return(dados)
