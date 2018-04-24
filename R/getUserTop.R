7e497af794f43b93ace2c49a1463a6a5f9288a11
Caleb Brooks <calebmbrooks@gmail.com>
2018-04-24 01:30:32 -0400

    screams of the unliving

--- R/getUserTop.R
+++ R/getUserTop.R
@@ -14,14 +14,13 @@ getUserTop<-function(artists=T,token){
     type <- "tracks"
   req <- httr::GET(paste0("https://api.spotify.com/v1/me/top/", type), httr::config(token = token))
   json1<-httr::content(req)
-  #json2<-jsonlite::fromJSON(jsonlite::toJSON(json1))$items
-  json2 <- json1
+  json2<-jsonlite::fromJSON(jsonlite::toJSON(json1))$items
   if(artists){
     dados=data.frame(id=json2$id,
                      name=json2$name,
                      popularity=json2$popularity,
-                     followers=json2$followers$total,
-                     genres=paste(json2$genres,collapse =";"))
+                     followers=json2$followers,
+                     genres=paste(json2$genres,collapse =","))
   }
   else{
     dados=data.frame(id=json2$id,
