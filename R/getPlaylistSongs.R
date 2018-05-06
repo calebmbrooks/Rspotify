2430dc79cf17cd42edf856bec62f48c1a1314622
Caleb Brooks <calebmbrooks@gmail.com>
2018-05-05 20:54:13 -0400

    removed limit from get playlist tracks

--- R/getPlaylistSongs.R
+++ R/getPlaylistSongs.R
@@ -10,14 +10,14 @@
 
 #function to get playlists' songs
 getPlaylistSongs<-function(ownerid,playlistid,offset=0,token){
-  req<-httr::GET(paste0("https://api.spotify.com/v1/users/",ownerid,"/playlists/",playlistid,"/tracks?&limit=100&offset=",offset),httr::config(token = token))
+  req<-httr::GET(paste0("https://api.spotify.com/v1/users/",ownerid,"/playlists/",playlistid,"/tracks?&offset=",offset),httr::config(token = token))
   json1<-httr::content(req)
   json2<-jsonlite::fromJSON(jsonlite::toJSON(json1))$items
   tracks<-unlist(json2$track$name)
   popularity<-unlist(json2$track$popularity)
   id<-unlist(json2$track$id)
   artist<-unlist(lapply(seq(1:length(tracks)), function (x){return(data.frame(json2$track$artists[x])$name[1])}))
-  artist_full<-sapply(lapply(seq(1:length(tracks)), function (x){return(data.frame(json2$track$artists[[x]])$name)}), paste, collapse = " feat. ") 
+  artist_full<-sapply(lapply(seq(1:length(tracks)), function (x){return(data.frame(json2$track$artists[[x]])$name)}), paste, collapse = " feat. ")
   artistId<-unlist(lapply(seq(1:length(tracks)), function (x){return(data.frame(json2$track$artists[x])$id[1])}))
   album<-unlist(json2$track$album$name)
   albumId<-unlist(json2$track$album$id)
